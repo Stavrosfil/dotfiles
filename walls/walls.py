6955150866b0d055b9972ca444ea4f82168db5de
Stavros Filosidis <stavrosfil@gmail.com>
2019-08-24 00:33:43 +0300

    Fixed README bug

--- walls/walls.py
+++ walls/walls.py
@@ -1,6 +1,9 @@
 import os
 import re
 
+basepath = '.'
+prefix = 'wall_'
+
 
 def pl():
     print('----------------------------------')
@@ -10,6 +13,7 @@ def atoi(text):
     return int(text) if text.isdigit() else text
 
 
+# This is used for natural sorting of a string list.
 def natural_keys(text):
     """ Turn a string into a list of string and number chunks.
         "z23a" -> ["z", 23, "a"]
@@ -17,19 +21,46 @@ def natural_keys(text):
     return [atoi(c) for c in re.split(r'(\d+)', text)]
 
 
-def remove_correct_names(filesIterator, prefix):
+def files_to_rename(prefix, extensions):
+    walls = []
+    filesIterator = os.scandir(basepath)
     print('\nFiles not matching :')
     pl()
     for wall in filesIterator:
         if wall.is_file():
-            if not bool(re.match(prefix + r"[0-9]+\.(jpg|png)", wall.name)):
+            # The files to rename must be image files or be already named correctly.
+            if not bool(re.match(prefix + r"[0-9]+\." + extensions, wall.name)) and bool(re.match(r".+\." + extensions, wall.name)):
                 print(wall.name)
                 walls.append(wall.name)
     pl()
 
-# TODO: Fix bug when there are no files to update.
-# !: If there are no files to update, readme becomes empty.
-def saveReadme():
+    # Natural sort.
+    # Also see natsort package.
+    walls.sort(key=natural_keys)
+
+    return walls
+
+
+def rename_files(walls):
+    print('Preparing to rename image files :')
+    pl()
+    i = 1
+    for wall in walls:
+        suffix = os.path.splitext(wall)[1]
+        newName = prefix + str(i) + suffix
+        print(wall + '\t-->\t' + newName)
+        os.rename(wall, newName)
+        i += 1
+    pl()
+
+
+def saveReadme(prefix, extensions):
+    filesList = os.listdir(basepath)
+    filesList.sort(key=natural_keys)
+    readme = ''
+    for wall in filesList:
+        if bool(re.match(prefix + r"[0-9]+\." + extensions, wall)):
+            readme += '![img](' + wall + ')\n'
     print('Updating readme...')
     pl()
     f = open('README.md', 'w')
@@ -39,29 +70,16 @@ def saveReadme():
     pl()
 
 
-basepath = '.'
-prefix = 'wall_'
-filesIterator = os.scandir(basepath)
-walls = []
-i = 1
-readme = ''
+def main():
 
-remove_correct_names(filesIterator, prefix)
+    extensions = '(jpg|png)'
 
-# Natural sort.
-# Also see natsort package.
-walls.sort(key=natural_keys)
+    walls = files_to_rename(prefix, extensions)
 
-print('Preparing to rename image files :')
-pl()
-for wall in walls:
-    suffix = os.path.splitext(wall)[1]
-    if bool(re.match(r".+\.(jpg|png)", wall)):
-        newName = prefix + str(i) + suffix
-        print(wall + '\t-->\t' + newName)
-        os.rename(wall, newName)
-        readme += '![img](' + newName + ')\n'
-        i += 1
-pl()
+    rename_files(walls)
 
-saveReadme()
+    saveReadme(prefix, extensions)
+
+
+if __name__ == "__main__":
+    main()
