2ddcd350784a55b19b0975c63e2f93b95277ecd8
Stavros Filosidis <stavrosfil@gmail.com>
2019-08-23 17:31:25 +0300

    Renaming script works

--- walls/walls.py
+++ walls/walls.py
@@ -1,24 +1,34 @@
 import os
-import fnmatch
-# entries = os.scandir('.')
-wallsList = os.listdir('.')
+import re
+# filesIterator = os.scandir('.')
+# filesIterator = os.listdir('.')
 
 # for entry in entries:
 # print(entry.name)
 
-# Search for non image files and remove them from the list
-print('\nTo be removed :')
-for wall in wallsList:
-    suffix = os.path.splitext(wall)[1]
-    # if not (fnmatch.fnmatch(wall, 'wall*.png') or fnmatch.fnmatch(wall, 'wall*.jpg')):
-    if not (suffix == '.jpg' or suffix == '.png'):
-        print(wall)
-        wallsList.remove(wall)
 
-print('\nImage files remaining : ' + str(len(wallsList)))
+def remove_correct_names(filesIterator, prefix):
+    print('\nTo be processed :')
+    for wall in filesIterator:
+        if wall.is_file():
+            if not bool(re.match(prefix+"[0-9]+.(jpg|png)", wall.name)):
+                print(wall.name)
+                walls.append(wall.name)
+    print('End of list creation.')
 
+
+basepath = '.'
+prefix = 'wall_'
+filesIterator = os.scandir(basepath)
+walls = []
 i = 0
 
-for wall in wallsList:
-    name = os.path.splitext(wall)
-    os.rename(wall, 'wall_' + str(i) + name[1])
+remove_correct_names(filesIterator, prefix)
+
+for wall in walls:
+    suffix = os.path.splitext(wall)[1]
+    if bool(re.match(r".+\.(jpg|png)", wall)):
+        newName = prefix + str(i) + suffix
+        print(newName)
+        os.rename(wall, newName)
+        i += 1
