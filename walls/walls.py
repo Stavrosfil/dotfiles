da2e9a1af64754fea59fec53bf146719850a07c3
Stavros Filosidis <stavrosfil@gmail.com>
2019-08-23 17:53:05 +0300

    Script improvements

--- walls/walls.py
+++ walls/walls.py
@@ -1,17 +1,11 @@
 import os
 import re
-# filesIterator = os.scandir('.')
-# filesIterator = os.listdir('.')
-
-# for entry in entries:
-# print(entry.name)
-
 
 def remove_correct_names(filesIterator, prefix):
     print('\nTo be processed :')
     for wall in filesIterator:
         if wall.is_file():
-            if not bool(re.match(prefix+"[0-9]+.(jpg|png)", wall.name)):
+            if not bool(re.match(prefix + r"[0-9]+.(jpg)", wall.name)):
                 print(wall.name)
                 walls.append(wall.name)
     print('End of list creation.')
@@ -22,13 +16,21 @@ prefix = 'wall_'
 filesIterator = os.scandir(basepath)
 walls = []
 i = 0
+readme = ''
 
 remove_correct_names(filesIterator, prefix)
 
 for wall in walls:
     suffix = os.path.splitext(wall)[1]
-    if bool(re.match(r".+\.(jpg|png)", wall)):
+    if bool(re.match(r".+\.(jpg)", wall)):
         newName = prefix + str(i) + suffix
         print(newName)
         os.rename(wall, newName)
+        readme += '![img](' + newName + ')\n'
         i += 1
+
+f = open('README.md', 'w')
+f.write(readme)
+f.close()
+
+print(readme)
