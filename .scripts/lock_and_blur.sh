cbcf164c17aebf3d2cc5c6f662250726ab7b8d3c
Stavros Filosidis <stavrosfil@gmail.com>
2020-03-10 22:43:46 +0200

    Fix polybar - Change locking - Change default terminal

--- .scripts/lock_and_blur.sh
+++ .scripts/lock_and_blur.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env sh
 
 # set the icon and a temporary location for the screenshot to be stored
-icon="$HOME/Pictures/firewatch.png"
-tmpbg='/tmp/screen.png'
+icon="$HOME/repos/dotfiles/walls/wall_20.png"
+# tmpbg='/tmp/screen.png'
 
 # take a screenshot
-scrot "$tmpbg"
+# scrot "$tmpbg"
 
 # blur the screenshot by resizing and scaling back up
-convert "$tmpbg" -filter Gaussian -blur 0x8 "$tmpbg"
+# convert "$tmpbg" -filter Gaussian -blur 0x8 "$tmpbg"
 #convert "$icon" -filter Gaussian -blur 0x8 "$icon"
 
 # overlay the icon onto the screenshot
 #convert "$tmpbg" "$icon" -gravity center -composite "$tmpbg"
 
 # lock the screen with the blurred screenshot
-i3lock -i "$tmpbg"
-#i3lock -i "$icon"
+# i3lock -i "$tmpbg"
+i3lock -i "$icon"
